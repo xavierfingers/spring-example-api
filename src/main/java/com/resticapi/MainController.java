2d6f0853d5a09d40762457e65aa9f3a8c197d31a
Zain <xavierfingers@gmail.com>
2025-11-22 16:38:33 -0700

    Update MainController.java
    Make it launch an app

--- src/main/java/com/resticapi/MainController.java
+++ src/main/java/com/resticapi/MainController.java
@@ -2,11 +2,12 @@ package com.resticapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.awt.Frame;
 @RestController
 public class MainController {
   @GetMapping("/")
    public String msg() {
-    return "Spring is awesome!";
+     new Frame();
   }
    @GetMapping("/skibidi")
     public String skibidi() {
