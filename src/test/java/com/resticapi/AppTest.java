df50dae237f8fc205ea2d115e0685aa1687d8746
Zain <xavierfingers@gmail.com>
2025-11-23 19:58:41 -0700

    Update AppTest.java

--- src/test/java/com/resticapi/AppTest.java
+++ src/test/java/com/resticapi/AppTest.java
@@ -8,10 +8,6 @@ import org.junit.jupiter.api.Test;
  * Unit test for simple App.
  */
 public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
