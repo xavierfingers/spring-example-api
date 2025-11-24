8047cc6b5479c480951b9f9ecf12fb41017a76c6
Zain <xavierfingers@gmail.com>
2025-11-23 19:56:04 -0700

    Update MainController.java

--- src/main/java/com/resticapi/MainController.java
+++ src/main/java/com/resticapi/MainController.java
@@ -1,8 +1,6 @@
 package com.resticapi;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.awt.Frame;
 @RestController
 public class MainController {
   @GetMapping("/")
